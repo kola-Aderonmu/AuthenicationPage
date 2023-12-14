4883dda9b639f898f2805a8e982e792a99bacadc
Adekola Aderonmu <gabkola94@gmail.com>
2023-12-14 12:15:17 +0100

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-const express = require('express')
-
-const app= express()
-
-// const port = process.env.PORT || 5000; //geneting  anew port 
-
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const app = express();
+// const port = process.env.PORT || 5000; //geneting  anew port
 const port = 5000;
+
+//---------------------------------------------------------
+
+// app.use(express.json()); //use to access object
+app.use(morgan("combined")); //use to get info on all info of a path
+app.use(express.static(path.resolve(__dirname, "login/login.html")))
+
+// middleware
+// const timeLogger = (req, res, next) => {
+//     const name ="kola";
+//     console.log(`"request Time:" ${Date.now()}`);
+//     if (name !== "yomi") {
+//         res.send("not authorised!")
+//     } else {
+//         next();
+//     }
+// };
+
+// app.use(req, res) => {
+//     console.log("Time:", Date.now());
+//     next()
+// }
+
+//first route
+
+app.get("/", (req, res) => {
+  console.log(path.resolve(__dirname, "login/login.html"));
+  res.sendFile(path.resolve(__dirname, "login/login.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.send("Welcome to my About Page");
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
---
+++ public/login.js
@@ -0,0 +1,1 @@
+console.log("login in the browser")
